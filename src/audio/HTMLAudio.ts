570086ec75398eaa418d182378d5aafc47e5f9e5
gelan <gelan>
2020-05-19 02:28:25 +0800

    音频

--- src/audio/HTMLAudio.ts
+++ src/audio/HTMLAudio.ts
@@ -1,18 +1,22 @@
-import { Emitter } from '../base/Emitter';
-import { AudioController } from './AudioController';
+import { Emitter } from "../base/Emitter";
+import { HTMLAudioElementProxy } from "./HTMLAudioElementProxy";
 
-/** 过程枚举 */
-export enum EnumAudioProcess {
-    START = "start",
-    STOP = "stop",
-    PAUSE = "pause",
-    END = "end",
-    PROGRESS = "progress",
-    ERROR = "error",
+/** 音频事件 */
+export enum EnumAudioEvent {
+    ON_PLAY = "ON_PLAY",
+    ON_STOP = "ON_STOP",
+    ON_PAUSE = "ON_PAUSE",
+    ON_END = "ON_END",
+    ON_PROGRESS = "ON_PROGRESS",
+    ON_ERROR = "ON_ERROR",
+
+    ON_MUTED_CHANGE = "ON_MUTED_CHANGE",
+    ON_VOLUME_CHANGE = "ON_VOLUME_CHANGE",
+    ON_LOOP_CHANGE = "ON_LOOP_CHANGE",
+    ON_CURRENT_TIME_CHANGE = "ON_CURRENT_TIME_CHANGE",
 }
 
-
-export class Audio extends Emitter {
+export class HTMLAudio extends Emitter {
     constructor(limit?: number) {
         super();
         if (limit != null && limit > 10) {
@@ -34,8 +38,8 @@ export class Audio extends Emitter {
         }
         value = !!value;
         this._muted = value;
-        this._audioList.forEach((ctrl: AudioController) => {
-            ctrl.muted = value;
+        this._audioList.forEach((proxy: HTMLAudioElementProxy) => {
+            proxy.muted = value;
         });
     }
     /** 获取静音 */
@@ -43,9 +47,8 @@ export class Audio extends Emitter {
         return this._muted;
     }
 
-
     /** 所有音频标签 */
-    private _audioList!: Array<AudioController>;
+    private _audioList!: Array<HTMLAudioElementProxy>;
 
     private _unlocked = false;
     /** 获取是否解锁 */
@@ -61,11 +64,10 @@ export class Audio extends Emitter {
         for (let i = 0; i < this._limit; i += 1) {
             const tag = document.createElement("audio");
             tag.load();
-            const ctrl = new AudioController(tag, i);
-            audioList.push(ctrl);
+            const proxy = new HTMLAudioElementProxy(tag, i);
+            audioList.push(proxy);
         }
         this._unlocked = true;
         this._audioList = audioList;
     }
-
 }
---
+++ src/audio/HTMLAudioElementProxy.ts
@@ -0,0 +1,131 @@
+import { Emitter } from "../base/Emitter";
+import { EnumAudioEvent } from "./HTMLAudio";
+
+/** 音频代理 */
+export class HTMLAudioElementProxy extends Emitter {
+    constructor(tag: HTMLAudioElement, uid: number) {
+        super();
+        this._uid = uid;
+        this._el = tag as AudioTag;
+        this.onplay = (evt: Event) => {
+            this.emit(EnumAudioEvent.ON_PLAY, evt);
+        };
+        this.onpause = (evt: Event) => {
+            const el = evt.target as AudioTag;
+            if (el.isStop) {
+                el.isStop = false;
+                this.emit(EnumAudioEvent.ON_STOP, evt);
+            } else {
+                this.emit(EnumAudioEvent.ON_PAUSE, evt);
+            }
+        };
+        this.ontimeupdate = (evt: Event) => {
+            const el = evt.target as AudioTag;
+            this.emit(EnumAudioEvent.ON_PROGRESS, el.currentTime, el.duration);
+        };
+        this.onended = (evt: Event) => {
+            this.emit(EnumAudioEvent.ON_END, evt);
+        };
+        this.onerror = (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error) => {
+            this.emit(EnumAudioEvent.ON_ERROR, { event, source, lineno, colno, error });
+        };
+    }
+
+    private _uid: number;
+    /** 获取唯一id */
+    get uid(): number {
+        return this._uid;
+    }
+
+    private _el: AudioTag;
+    /** 设置源 */
+    set src(value: string) {
+        const el = this._el;
+        if (el.src) {
+            if (el.src === value) {
+                return;
+            }
+            this.stop();
+            el.src = value;
+        } else {
+            el.src = value;
+        }
+    }
+
+    /** 循环播放 */
+    get loop(): boolean {
+        return this._el.loop;
+    }
+    /** 循环播放 */
+    set loop(value: boolean) {
+        this._el.loop = value;
+        this.emit(EnumAudioEvent.ON_LOOP_CHANGE, value);
+    }
+
+    /** 音量 */
+    get volume(): number {
+        return this._el.volume;
+    }
+    /** 音量 */
+    set volume(value: number) {
+        this._el.volume = value;
+        this.emit(EnumAudioEvent.ON_VOLUME_CHANGE, value);
+    }
+
+    /** 是否静音 */
+    get muted(): boolean {
+        return this._el.muted;
+    }
+    /** 是否静音 */
+    set muted(value: boolean) {
+        this._el.muted = value;
+        this.emit(EnumAudioEvent.ON_MUTED_CHANGE, value);
+    }
+
+    /** 音频长度 */
+    get duration(): number {
+        return this._el.duration;
+    }
+
+    /** 当前播放进度 */
+    get currentTime(): number {
+        return this._el.currentTime;
+    }
+    /** 当前播放进度 */
+    set currentTime(value: number) {
+        this._el.currentTime = value;
+        this.emit(EnumAudioEvent.ON_CURRENT_TIME_CHANGE, value);
+    }
+
+    /** 播放 */
+    play(): void {
+        this._el.play();
+    }
+
+    /** 停止 */
+    stop(): void {
+        this._el.isStop = true;
+        this._el.pause();
+        this.currentTime = 0;
+    }
+
+    /** 暂停 */
+    pause(): void {
+        this._el.pause();
+    }
+
+    /** 恢复播放 */
+    resume(): void {
+        this._el.play();
+    }
+
+    protected ontimeupdate: (evt: Event) => void;
+    protected onplay: (evt: Event) => void;
+    protected onpause: (evt: Event) => void;
+    protected onended: (evt: Event) => void;
+    protected onerror: (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error) => void;
+}
+
+interface AudioTag extends HTMLAudioElement {
+    isStop: boolean;
+}
